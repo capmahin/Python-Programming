8c6789ba1333b082c0cf652fabde4698df02d2b2
capmahin <mahinulhuque1996@gmail.com>
2025-08-06 22:25:29 +0600

    creating ifels

---
+++ if_elif.py
@@ -0,0 +1,10 @@
+age = int(input('Enter your age: '))
+
+if age < 10:
+    print('you are young, strange one')
+
+elif age < 40:
+    print('the fire in you is strong, strange one')
+
+else:
+    print('you are wise beyond doubt, strange one')
