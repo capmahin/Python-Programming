14fc1cfe203f4bfd4aa2396ac735e97650f9f1a3
capmahin <mahinulhuque1996@gmail.com>
2025-08-09 03:17:37 +0600

    add range(len)

--- ranges.py
+++ ranges.py
@@ -1,5 +1,10 @@
 # for n in range(3, 10):
 #     print(n)
 
-for n in range(0, 20, 4):
-    print(n)
+# for n in range(0, 20, 4):
+#     print(n)
+
+burgers = ['beef', 'chicken', 'veg', 'supreme', 'double']
+
+for n in range(len(burgers)):
+    print(n, burgers[n])
