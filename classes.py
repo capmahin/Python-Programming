14268ae8454e0330d2c07603ff78522f8d61a1f6
capmahin <mahinulhuque1996@gmail.com>
2025-08-23 20:29:15 +0600

    add hot

--- classes.py
+++ classes.py
@@ -1,13 +1,16 @@
 class Planet:
-    def __init__(self):
-        self.name = 'Hoth'
-        self.radius = '200000'
-        self.gravity = 5.5
-        self.system = 'Hoth System'
+    def __init__(self, name, radius, gravity, system):
+        self.name = name
+        self.radius = radius
+        self.gravity = gravity
+        self.system = system
+
+    def orbit(self):
+        return f'{self.name} is orbiting in the {self.system}'
 
 
-hoth = Planet()
+hoth = Planet('Hoth', 200000, 5.5, 'Hoth System')
 print(f'Name is: {hoth.name}')
 print(f'Radius is: {hoth.radius}')
 print(f'Gravity is: {hoth.gravity}')
-print(f'System is: {hoth.system}')
+print(hoth.orbit())
